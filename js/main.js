a6fa2eb376619d3e0b57529e42b858e711a014b1
Rapieree <serega.mironov11@gmail.com>
2021-02-05 19:44:17 +0300

    Написал функции для создания тестовых объявлений

--- js/main.js
+++ js/main.js
@@ -19,15 +19,79 @@ const getRandomNumb = (min, max, decimalSign = 0) => {
   return Number.isInteger(random) ? Math.round(random) : random;
 }
 
-// Проверка вероятности
-let minimum = 0;
-let maximum = 10;
-let decimal = 0;
-let arrCount = new Map();
+// // Проверка вероятности
+// let minimum = 0;
+// let maximum = 10;
+// let decimal = 0;
+// let arrCount = new Map();
+// for(let i = 0; i < 100000; i++) {
+//   let temp = getRandomNumb(minimum, maximum, decimal);
+//   arrCount.has(temp) ? arrCount.set(temp, arrCount.get(temp) + 1) : arrCount.set(temp, 1);
+// }
+// arrCount.forEach((value, key) => console.log('Число = ' + key +', Совпадений = ' + value));
 
-for(let i = 0; i < 100000; i++) {
-  let temp = getRandomNumb(minimum, maximum, decimal);
-  arrCount.has(temp) ? arrCount.set(temp, arrCount.get(temp) + 1) : arrCount.set(temp, 1);
+const TYPE_DWELLING = ['palace', 'flat', 'house', 'bungalow'];
+const CHECK_TIME = ['12:00', '13:00', '14:00'];
+const FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const TEMP_TITLE = 'Милая, уютная халупа на окраине Питера';
+const TEMP_DESCRIPTION = 'Мебель: кровать, стол и стулья, шкаф для одежды. ' +
+  'Микроволновка, чайник, холодильник, плита. Утюг и гладильная доска.';
+const PHOTOS_LIST = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const getAuthor = () =>  ({ avatar: 'img/avatars/user0' + getRandomNumb(1, 8) + '.png' });
+
+const getFeatures = (featuresArr) => {
+  let startIndex, endIndex;
+  do {
+    startIndex =  getRandomNumb(0, featuresArr.length-1);
+    endIndex = getRandomNumb(1, featuresArr.length);
+  } while(startIndex > endIndex);
+  return featuresArr.slice(startIndex, endIndex);
 }
 
-arrCount.forEach((value, key) => console.log('Число = ' + key +', Совпадений = ' + value));
+const getPhotos = (photosArr, maxPhotos) => {
+  let tempArr = new Array(getRandomNumb(1, maxPhotos)).fill(null);
+  tempArr.forEach((value, index) => {
+    tempArr[index] = photosArr[getRandomNumb(0, photosArr.length-1)];
+  });
+  return tempArr;
+}
+
+const getLocation = () => {
+  return {
+    x: getRandomNumb(35.6, 35.7, 5),
+    y: getRandomNumb(139.7, 139.8, 5),
+  }
+}
+
+const getOffer = (AdressCoord) => {
+  return {
+    title: TEMP_TITLE,
+    adress: Object.values(AdressCoord).join(', '),
+    price: getRandomNumb(0, 100000),
+    type: TYPE_DWELLING[getRandomNumb(0, TYPE_DWELLING.length-1)],
+    rooms: getRandomNumb(0, 10),
+    guests: getRandomNumb(0, 30),
+    checkin: CHECK_TIME[getRandomNumb(0, CHECK_TIME.length-1)],
+    checkout: CHECK_TIME[getRandomNumb(0, CHECK_TIME.length-1)],
+    features: getFeatures(FEATURES_LIST),
+    description: TEMP_DESCRIPTION,
+    photos: getPhotos(PHOTOS_LIST, 10),
+  }
+}
+
+const getAdvert = (locationAdress) => {
+  return {
+    author: getAuthor(),
+    offer: getOffer(locationAdress),
+    location: locationAdress,
+  }
+}
+
+const advertsArray = new Array(10).fill(null).map(() => getAdvert(getLocation()));
+
+console.log(advertsArray);
