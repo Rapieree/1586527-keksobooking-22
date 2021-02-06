b6a1f5bbad9b3321fcffecb7560706202bc32f8f
Rapieree <serega.mironov11@gmail.com>
2021-02-06 18:45:12 +0300

    Исправил функции для создания тестовых объявлений(изменил имена констант)

--- js/main.js
+++ js/main.js
@@ -30,10 +30,14 @@ const getRandomNumb = (min, max, decimalSign = 0) => {
 // }
 // arrCount.forEach((value, key) => console.log('Число = ' + key +', Совпадений = ' + value));
 
-const TYPE_DWELLING = ['palace', 'flat', 'house', 'bungalow'];
-const CHECK_TIME = ['12:00', '13:00', '14:00'];
+const TYPES_OF_DWELLING = ['palace', 'flat', 'house', 'bungalow'];
+const CHECK_TIMES = ['12:00', '13:00', '14:00'];
 const FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const TEMP_TITLE = 'Милая, уютная халупа на окраине Питера';
+const LATITUDE_MIN = 35.6;
+const LATITUDE_MAX = 35.7;
+const LONGITUDE_MIN = 139.7;
+const LONGITUDE_MAX = 139.8;
 const TEMP_DESCRIPTION = 'Мебель: кровать, стол и стулья, шкаф для одежды. ' +
   'Микроволновка, чайник, холодильник, плита. Утюг и гладильная доска.';
 const PHOTOS_LIST = [
@@ -41,6 +45,7 @@ const PHOTOS_LIST = [
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
+
 
 const getAuthor = () =>  ({ avatar: 'img/avatars/user0' + getRandomNumb(1, 8) + '.png' });
 
@@ -63,21 +68,21 @@ const getPhotos = (photosArr, maxPhotos) => {
 
 const getLocation = () => {
   return {
-    x: getRandomNumb(35.6, 35.7, 5),
-    y: getRandomNumb(139.7, 139.8, 5),
+    x: getRandomNumb(LATITUDE_MIN, LATITUDE_MAX, 5),
+    y: getRandomNumb(LONGITUDE_MIN, LONGITUDE_MAX, 5),
   }
 }
 
-const getOffer = (AdressCoord) => {
+const getOffer = (AddressCoord) => {
   return {
     title: TEMP_TITLE,
-    adress: Object.values(AdressCoord).join(', '),
+    adress: Object.values(AddressCoord).join(', '),
     price: getRandomNumb(0, 100000),
-    type: TYPE_DWELLING[getRandomNumb(0, TYPE_DWELLING.length-1)],
+    type: TYPES_OF_DWELLING[getRandomNumb(0, TYPES_OF_DWELLING.length-1)],
     rooms: getRandomNumb(0, 10),
     guests: getRandomNumb(0, 30),
-    checkin: CHECK_TIME[getRandomNumb(0, CHECK_TIME.length-1)],
-    checkout: CHECK_TIME[getRandomNumb(0, CHECK_TIME.length-1)],
+    checkin: CHECK_TIMES[getRandomNumb(0, CHECK_TIMES.length-1)],
+    checkout: CHECK_TIMES[getRandomNumb(0, CHECK_TIMES.length-1)],
     features: getFeatures(FEATURES_LIST),
     description: TEMP_DESCRIPTION,
     photos: getPhotos(PHOTOS_LIST, 10),
